4ac29e2f6573c4a9ade96e7c06ec99571fe0a03f
sagar.shankhala <sagar.shankhala@publicissapient.com>
2021-07-23 10:58:18 +0530

    first commit

---
+++ src/main/java/com/foobar/BarDbConfig.java
@@ -0,0 +1,58 @@
+package com.foobar;
+
+import javax.persistence.EntityManagerFactory;
+import javax.sql.DataSource;
+
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceBuilder;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.orm.jpa.EntityManagerFactoryBuilder;
+import org.springframework.boot.orm.jpa.hibernate.SpringImplicitNamingStrategy;
+import org.springframework.boot.orm.jpa.hibernate.SpringPhysicalNamingStrategy;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.orm.jpa.JpaTransactionManager;
+import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
+import org.springframework.transaction.PlatformTransactionManager;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import static java.util.Collections.singletonMap;
+
+@Configuration
+@EnableTransactionManagement
+@EnableJpaRepositories(entityManagerFactoryRef = "barEntityManagerFactory",
+        transactionManagerRef = "barTransactionManager", basePackages = {"com.foobar.bar.repo"})
+public class BarDbConfig {
+
+    @Bean(name = "barDataSource")
+    @ConfigurationProperties(prefix = "bar.datasource")
+    public DataSource dataSource() {
+        return DataSourceBuilder.create().build();
+    }
+
+    @Bean(name = "barEntityManagerFactory")
+    public LocalContainerEntityManagerFactoryBean barEntityManagerFactory(
+
+
+      EntityManagerFactoryBuilder builder, @Qualifier("barDataSource") DataSource dataSource) {
+        Map<String, String> map = new HashMap<>();
+        map.put("hibernate.ddl-auto", "update");
+        map.put("hibernate.dialect", "org.hibernate.dialect.PostgreSQL94Dialect");
+
+        return builder.dataSource(dataSource).packages("com.foobar.bar.domain").persistenceUnit("bar").properties(
+                map
+        )
+                .build();
+    }
+
+    @Bean(name = "barTransactionManager")
+    public PlatformTransactionManager barTransactionManager(
+            @Qualifier("barEntityManagerFactory") EntityManagerFactory barEntityManagerFactory) {
+        return new JpaTransactionManager(barEntityManagerFactory);
+    }
+
+}
